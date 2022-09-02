99b39ea07161440daeb59941d8a2f83dff7c22f5
Ant <acount15104@gmail.com>
2022-09-02 06:23:05 -0700

    finished setting up workspace working on reseting dbs

--- Backend.js
+++ Backend.js
@@ -4,7 +4,9 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 3000
 
-
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.json())
 
 
 app.listen(PORT, () => {
